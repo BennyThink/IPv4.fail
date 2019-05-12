085d33f657d52bdf531426793cb56791a7ece0c5
Benny <benny.think@gmail.com>
2019-05-12 20:51:50 +0800

    add bootstrap stuffs

--- main.py
+++ main.py
@@ -17,86 +17,8 @@ from tornado.web import HTTPError
 
 
 class BaseHandler(web.RequestHandler):
-    executor = ThreadPoolExecutor(max_workers=20)
-
     def data_received(self, chunk):
         pass
-
-    @staticmethod
-    def response_func():
-        pass
-
-    @staticmethod
-    def sync_func():
-        pass
-
-    @run_on_executor
-    def run_request(self):
-        return json.dumps(self.response_func())
-
-    @gen.coroutine
-    def get(self):
-        if not self.get_cookie('_xsrf'):
-            self.set_cookie('_xsrf', self.xsrf_token, expires_days=7)
-        self.set_header("Content-Type", "application/json")
-        res = yield self.run_request()
-        self.write(res)
-
-    @run_on_executor
-    def post(self):
-        try:
-            self.get_argument('refresh')
-            self.sync_func()
-            self.set_status(201)
-        except HTTPError as e:
-            self.set_status(400)
-            self.write({"code": 400001, "message": CODE.get(400001), "error": str(e)})
-        except SSHException as e:
-            self.set_status(504)
-            self.write({"code": 504001, "message": CODE.get(504001), "error": str(e)})
-        except (AttributeError, TypeError) as e:
-            self.set_status(504)
-            self.write({"code": 504002, "message": CODE.get(504002), "error": str(e)})
-        except error as e:
-            self.set_status(500)
-            self.write({"code": 500001, "message": CODE.get(500001), "error": str(e)})
-        except Exception as e:
-            self.set_status(500)
-            self.write({"code": 500099, "message": CODE.get(500099), "error": str(e)})
-
-    @staticmethod
-    def _time_independent_equals(a, b):
-        if len(a) != len(b):
-            return False
-        result = 0
-        if isinstance(a[0], int):  # python3 byte strings
-            for x, y in zip(a, b):
-                result |= x ^ y
-        return result == 0
-
-    def check_xsrf_cookie(self):
-        token = (self.get_argument("_xsrf", None) or
-                 self.request.headers.get("X-Xsrftoken") or
-                 self.request.headers.get("X-Csrftoken"))
-        if not token:
-            self.set_status(403)
-            self.write({"code": 403001, "message": CODE.get(403001), "error": CODE.get(403001)})
-            self.finish()
-            raise HTTPError(403, "'_xsrf' argument missing from POST")
-
-        _, token, _ = self._decode_xsrf_token(token)
-        _, expected_token, _ = self._get_raw_xsrf_token()
-        if not token:
-            self.set_status(403)
-            self.write({"code": 403002, "message": CODE.get(403002), "error": CODE.get(403002)})
-            self.finish()
-            raise HTTPError(403, "'_xsrf' argument has invalid format")
-
-        if not self._time_independent_equals(utf8(token), utf8(expected_token)):
-            self.set_status(403)
-            self.write({"code": 403003, "message": CODE.get(403003), "error": CODE.get(403003)})
-            self.finish()
-            raise HTTPError(403, "XSRF cookie does not match POST argument")
 
 
 class IndexHandler(BaseHandler):
@@ -104,41 +26,38 @@ class IndexHandler(BaseHandler):
         self.render("pages/index.html")
 
 
-class LoginHandler(BaseHandler):
-    def data_received(self, chunk):
-        pass
+class WeatherHandler(BaseHandler):
+    executor = ThreadPoolExecutor(max_workers=20)
 
+    @run_on_executor
+    def run_request(self):
+        """
+        sign and return
+        :return: hex and raw request in XML
+        """
+        # get parameter, compatibility with json
+
+
+        if self.request.headers.get('Content-Type') == 'application/json' and self.request.body:
+            data = json.loads(self.request.body)
+            city = data.get('city')
+            day = data.get('day')
+        # return whole json.
+        else:
+            response = make_json(city)
+        # set http code and response
+        self.set_status(HTTP.get(response.get('code'), 418))
+        return response
+
+    @gen.coroutine
     def get(self):
-        self.set_status(405)
-        self.write({"code": 405001, "message": CODE.get(405001), "error": CODE.get(405001)})
+        res = yield self.run_request()
+        self.write(res)
 
+    @gen.coroutine
     def post(self):
-        try:
-            password = self.get_argument('password')
-            result = PassAuth('ss_auth').verify_pass(password)
-            if result:
-                self.set_status(200)
-                self.set_secure_cookie('password', password, expires_days=7, httponly=True)
-                self.write({"code": 200002, "message": CODE.get(200002), "error": CODE.get(200002)})
-            else:
-                raise AttributeError('Your password didn\'t pass verification.')
-        except HTTPError as e:
-            self.set_status(401)
-            self.write({"code": 401002, "message": CODE.get(401002), "error": str(e)})
-        except AttributeError as e:
-            self.set_status(401)
-            self.write({"code": 401001, "message": CODE.get(401001), "error": str(e)})
-
-
-class WebStatusHandler(BaseHandler):
-    @staticmethod
-    def response_func():
-        return web_response()
-
-    @staticmethod
-    def sync_func():
-        return web_sync()
-
+        res = yield self.run_request()
+        self.write(res)
 
 
 class RunServer:
@@ -155,7 +74,7 @@ class RunServer:
                 ]
     settings = {
         "static_path": os.path.join(root_path, "static"),
-        "cookie_secret": "5Li05DtnQewDZqpmDVB3HAAhFqUu2vDnUSnqezkeu+M=",
+        "cookie_secret": "5Li05DtnQewDZq1mDVB3HAAhFqUu2vD2USnqezkeu+M=",
         "xsrf_cookies": True,
         "autoreload": True
     }
@@ -182,7 +101,7 @@ class RunServer:
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    options.define("p", default=8888, help="running port", type=int)
+    options.define("p", default=6789, help="running port", type=int)
     options.define("h", default='127.0.0.1', help="listen address", type=str)
     options.parse_command_line()
     p = options.options.p
